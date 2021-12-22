efe96f3ca32fbc51427d545d7831b27d74063a94
tchomicz <tchomicz@pgs-soft.com>
2021-12-22 14:29:51 +0100

    Init Project

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import TheTopNavMenu from './components/TheTopNavMenu.vue';
+</script>
+
+
+<template>
+  <the-top-nav-menu />
+  <main id="#main">
+    <router-view/>
+  </main>
+
+</template>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TheTopNavMenu.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <router-link to="/"><img src="@/assets/logo.png" alt="Back to home page"/></router-link>
+  <nav aria-label="Main navigation">
+    <ul>
+      <li>
+        <router-link to="/"> Insurence</router-link>
+      </li>
+      <li>
+        <router-link to="/"> About us</router-link>
+      </li>
+      <li>
+        <router-link to="/"> Claim report</router-link>
+      </li>
+      <li>
+        <router-link to="/"> Contact</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+
+</style>
